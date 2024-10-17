da4cfb93f4417231716281e618724917bd911381
Votre Nom <votre.email@example.com>
2024-10-17 19:39:37 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'portfolio';
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_5rojrjk', 'template_vocz3hr', e.target as HTMLFormElement, 'MenLOkOOO3JnDBx4Y')
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log('SUCCESS!', result.status, result.text);
+        },
+        (error) => {
+          console.log('FAILED...', error);
+        }
+      );
+  }
 }
