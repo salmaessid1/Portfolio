9f644812dfafa26453288dcd29270554815fc992
Votre Nom <votre.email@example.com>
2024-11-02 14:33:37 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        AboutComponent,
     ],
     imports: [
         BrowserModule,
